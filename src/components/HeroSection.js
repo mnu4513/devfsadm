b41751dedaad4f30dc1a839c43e3ad8c0942f25c
mnu4513 <mnu4513@gmail.com>
2024-03-06 00:05:19 +0530

    .

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -3,8 +3,21 @@ import business_img from '../assets/img/business.svg';
 import { Link } from 'react-router-dom';
 import iso_icon from '../assets/ico/iso.svg';
 import dev_icon from '../assets/ico/dev.svg';
+import { ChevronDownIcon } from '@heroicons/react/20/solid';
+import DropDown from './utils/DropDown';
 
 const HeroSection = () => {
+
+    const courses = [
+        { name: 'Solaris', description: 'Get a better understanding of your traffic', href: '/course/solaris', icon: ChevronDownIcon },
+        { name: 'Linux', description: 'Speak directly to your customers', href: '/course/solaris', icon: ChevronDownIcon },
+        { name: 'Git', description: 'Your customers’ data will be safe and secure', href: '/course/solaris', icon: ChevronDownIcon },
+        { name: 'HTML', description: 'Connect with third-party tools', href: '/course/solaris', icon: ChevronDownIcon },
+        { name: 'JavaScript', description: 'Build strategic funnels that will convert', href: '/course/solaris', icon: ChevronDownIcon },
+        { name: 'React', description: 'Build strategic funnels that will convert', href: '/course/solaris', icon: ChevronDownIcon },
+    ];
+
+
     return (
         <section >
             <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
@@ -17,14 +30,16 @@ const HeroSection = () => {
                         <br className="hidden md:inline lg:hidden" /> in a easy and intresting way
                     </p>
                     <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
-                        <Link rel="noopener noreferrer" to='/administration' className="flex flex-row px-8 py-4 text-lg font-semibold rounded dark:bg-violet-400 dark:text-gray-900">
+                        {/* <Link rel="noopener noreferrer" to='/administration' className="flex flex-row px-8 py-4 text-lg font-semibold rounded dark:bg-violet-400 dark:text-gray-900">
                             <img src={iso_icon} alt='' className='mr-3  rounded-full h-8 bg-white' />
                             Administration
-                        </Link>
-                        <Link rel="noopener noreferrer" to='/development' className="flex flex-row px-8 py-3 text-lg font-semibold border rounded ">
-                            <img src={dev_icon} alt='' className='mr-3  rounded-full h-8 bg-white' />
-                            Development
-                        </Link>
+                        </Link> */}
+                        <button className="flex flex-row text-lg font-semibold rounded dark:bg-violet-400 dark:text-gray-900">
+                            <DropDown icon={iso_icon} name='Administration' content={courses} />
+                        </button>
+                        <button className="flex flex-row  text-lg font-semibold border rounded ">
+                            <DropDown icon={dev_icon} name='Development' content={courses} />
+                        </button>
                     </div>
                 </div>
                 <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
