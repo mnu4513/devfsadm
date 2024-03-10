08e135cd5da943d2ecf26ded642f796558c8f5cc
mnu4513 <mnu4513@gmail.com>
2024-03-10 13:29:55 +0530

    .

--- src/components/UploadImage.js
+++ src/components/UploadImage.js
@@ -34,9 +34,9 @@ const UploadImage = () => {
     };
 
     return (
-        <div>
-            <input type="file" onChange={handleImageChange} />
-            <button onClick={handleUpload}>Upload Image</button>
+        <div className='flex flex-col max-w-fit mx-auto space-y-6 my-6 border px-4 py-3 border-gray-600 rounded-md'>
+            <input type="file" onChange={handleImageChange} className='border py-2 px-3 border-gray-600 rounded-md'/>
+            <button onClick={handleUpload} className='border border-gray-600 rounded-md px-3 py-2  max-w-fit mx-auto'>Upload Image</button>
         </div>
     );
 };
