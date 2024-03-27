a73ffc7a04dd56891083e529713eb03b52a9cee1
mnu4513 <mnu4513@gmail.com>
2024-03-27 22:45:14 +0530

    .

--- src/components/Config.js
+++ src/components/Config.js
@@ -1,1 +1,5 @@
 export const img_base_url = 'https://res.cloudinary.com/dyjpzvstq/image/upload/v1709985632/';
+
+export const logoH = 'egimzor3enkp5n94n8vi';
+
+export const mine = 'icrsrytyvc0mesnvz6qy';
