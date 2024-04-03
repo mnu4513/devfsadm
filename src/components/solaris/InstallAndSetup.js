a59afe4b7aa916e7913953ca12567dc29311e536
mnu4513 <mnu4513@gmail.com>
2024-04-04 01:00:32 +0530

    .

--- src/components/solaris/InstallAndSetup.js
+++ src/components/solaris/InstallAndSetup.js
@@ -4,9 +4,9 @@ import drive_icon from '../../assets/ico/drive_icon.webp';
 
 const InstallAndSetup = () => (
   <div className='flex flex-col px-5 space-y-5 lg:max-w-6xl mx-auto mb-5'>
-    <h3 className='text-2xl my-5 font-bold'>Installation and setup</h3>
+    <h3 className='text-2xl text-red-600 my-5 font-bold'>Installation and setup</h3>
     <div className='ml-2'>
-      <h2 className='text-xl font-bold'> Tools we required </h2>
+      <h2 className='text-xl text-red-600 font-bold'> Tools we required </h2>
       <p className='mt-2'> Download by visiting official website or on telegram or on google drive </p>
       <ul className='ml-8 space-y-2 mt-2'>
         <button className='flex flex-row w-60 border-2 px-2 py-1 items-center justify-between rounded-md bg-gray-300 text-blue-400'>    <span> Oracle Solaris 11 </span>
@@ -25,7 +25,7 @@ const InstallAndSetup = () => (
       </ul>
     </div>
     <div className='ml-2'>
-      <h2 className='text-xl font-bold mt-4  '> Process to Installation </h2>
+      <h2 className='text-xl text-red-600 font-bold mt-4  '> Process to Installation </h2>
       <p className='mt-2 '>
         Fitst of all we need to download the ISO image file of Oracle Solaris 11.
         Then we need to download VMware or Virtual Box to run ISO image on our windows pc or on linux or macbook.
@@ -34,12 +34,12 @@ const InstallAndSetup = () => (
       <p className='mt-2'> After the Installation of VMware, open VMware and start the setup. If you are having any issue in setup, you can refer the video for setup and isntallation.</p>
     </div>
     <div >
-      <iframe className='mt-5 h-96 w-full md:h-96 md:px-24 lg:w-4/5' src="https://www.youtube.com/embed/C49Tksl_3J4?si=4GLRa9_QsoRG_LGf" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+      <iframe className='mt-5 h-96 w-full md:h-96 md:px-24 lg:w-4/5' src="https://www.youtube.com/embed/tomhp3foV60?si=blSVlviON0h60rJl" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
     </div>
 
 
     <div>
-      <p className='mt-2 ml-2'> While installing VMware pro 17 or higher version of VMware pro user key for pro version <span className='font-bold' >'MC60H-DWHD5-H80U9-6V85M-8280D'</span> </p>
+      <p className='mt-2 ml-2'> While installing VMware pro 17 or higher version of VMware pro user key for pro version <span className='font-bold text-blue-400' >'MC60H-DWHD5-H80U9-6V85M-8280D'</span> </p>
 
       <p className='mt-2 ml-2'>After downloading and installing VMware we will install Solaris iso file on this app.
         For this we won't use advance custom setting, we will use normal setting provided by VMware application.
--- src/components/solaris/IntroToSolaris.js
+++ src/components/solaris/IntroToSolaris.js
@@ -5,39 +5,39 @@ const IntroToSolaris = () => {
   return (
     <div className='mx-4 my-4 lg:max-w-7xl lg:mx-auto'>
       <div>
-        <h2 className='text-2xl font-bold mt-4 ml-4 mb-3'>Solaris</h2>
+        <h2 className='text-2xl text-red-600 font-bold mt-4 ml-4 mb-3'>Solaris</h2>
         <p className='ml-4'>  Solaris is a Unix-based operating system (OS) developed by Sun Microsystems, which is now owned by Oracle Corporation. Solaris has a long history and has been widely used in enterprise environments for server applications, especially in areas such as data centers and large-scale computing systems.</p>
       </div>
       <div >
-        <iframe className='mt-5 h-96 w-full md:h-96 md:px-24 lg:w-4/5' src="https://www.youtube.com/embed/C49Tksl_3J4?si=4GLRa9_QsoRG_LGf" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+        <iframe className='mt-5 h-96 w-full md:h-96 md:px-24 lg:w-4/5' src="https://www.youtube.com/embed/aIHWfk8K3G8?si=kesAdg1n1uqB1BKN" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
       </div>
       <div className='mt-5 ml-4'>
-        <h2 className='text-xl font-bold mb-4'> Key Features </h2>
-        <span className=' ml-2 font-bold '>1. Scalability </span>
+        <h2 className='text-xl font-bold text-red-600 mb-4'> Key Features </h2>
+        <span className='text-blue-500 ml-2 font-bold '>1. Scalability </span>
         <p className='ml-7 mb-4'>
           Solaris is designed to run on a wide range of hardware platforms, from small servers to large mainframes. It offers scalability to meet the needs of various computing environments.
         </p>
-        <span className=' ml-2 font-bold '>2. Reliability </span>
+        <span className='text-blue-500 ml-2 font-bold '>2. Reliability </span>
         <p className='ml-7 mb-4'>
           Solaris is known for its stability and reliability. It includes features such as dynamic tracing (DTrace) and advanced diagnostics tools to help administrators identify and troubleshoot issues.
         </p>
-        <span className='font-bold mt-2 ml-2'>3. Security </span>
+        <span className='text-blue-500 font-bold mt-2 ml-2'>3. Security </span>
         <p className='ml-7 mb-4'>
           Solaris incorporates various security features, including role-based access control (RBAC), cryptographic frameworks, and support for security standards. It is designed to meet the security requirements of enterprise environments.
         </p>
-        <span className='font-bold mt-2 ml-2'>4. Virtualization </span>
+        <span className='text-blue-500 font-bold mt-2 ml-2'>4. Virtualization </span>
         <p className='ml-7 mb-4'>
           Solaris includes built-in support for virtualization technologies such as Oracle VM Server for SPARC/x86 and the Solaris Containers (now called Oracle Solaris Zones), allowing for efficient resource utilization and isolation.
         </p>
-        <span className='font-bold mt-2 ml-2'>5. File Systems </span>
+        <span className='text-blue-500 font-bold mt-2 ml-2'>5. File Systems </span>
         <p className='ml-7 mb-4'>
           Solaris supports advanced file systems like ZFS (Zettabyte File System), which provides features such as data integrity, snapshotting, and easy administration.
         </p>
-        <span className='font-bold mt-2 ml-2'>6. Networking </span>
+        <span className='text-blue-500 font-bold mt-2 ml-2'>6. Networking </span>
         <p className='ml-7 mb-4'>
           Solaris has robust networking capabilities, supporting various network protocols and technologies. It includes features like TCP/IP networking, IPv6 support, and network virtualization.
         </p>
-        <span className='font-bold mt-2 ml-2'>7. Compatibility </span>
+        <span className='text-blue-500 font-bold mt-2 ml-2'>7. Compatibility </span>
         <p className='ml-7 mb-4'>
           Solaris has a long history of compatibility with industry standards, making it suitable for interoperability with other systems and applications.
         </p>
