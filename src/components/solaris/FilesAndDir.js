690a9f224d600b06e578d399f1c4ca9461494f7a
mnu4513 <mnu4513@gmail.com>
2025-02-13 00:20:07 +0530

    file and dir pending

--- src/components/solaris/FilesAndDir.js
+++ src/components/solaris/FilesAndDir.js
@@ -1,124 +1,148 @@
 import React from 'react';
-import { img_base_url } from '../Config';
-import YouTubeVideo from '../utils/YouTubeVideo';
+import { Heading, Subheading, Supersubheading, Text, TerminalOutput, Line } from "../utils/Comps";
 
 const FilesAndDir = () => {
-
   return (
-    <div className='mx-4 my-4 lg:max-w-7xl lg:mx-auto'>
-      <div>
-        <h2 className='text-2xl font-bold my-5 text-red-500 ml-4'>Files and Directories in Solaris</h2>
-        <p className='ml-4'>
-          In Solaris, service management involves the management of services, daemons, and system processes. Solaris uses the Service Management Facility (SMF) to manage system services. SMF replaces traditional init scripts and provides a more sophisticated and flexible mechanism for managing services.
-        </p>
-      </div>
+    <div className='mx-4 my-4 lg:max-w-4xl lg:mx-auto'>
+      <Heading text={`Files and Directories in Solaris`} />
 
-      <YouTubeVideo video_id={`yVwCHpj3aOI`} />
-   
+      <Text text={`In Solaris, managing files and directories involves various commands for creation, modification, listing, and removal. Here we will look at the essential commands for working with files and directories in Solaris.`} />
 
-      {/* files */}
-      <h2 className='ml-4 mr-2 mt-8 text-teal-500 font-bold text-xl'> Files </h2>
-      <div className='ml-4'>
-        <p className='mt-3 font-bold text-red-500'>1. To create an empty file</p>
-        <p className=' font-bold ml-5 text-violet-600'> {`touch <file_name>`} </p>
-        <img src={`${img_base_url}hxcpxfe1vdbcxcu2peaa.png`} alt='' className='mt-5 mb-8 md:w-2xl lg:max-w-5xl' />
-      </div>
+      {/* Files */}
+      <Subheading text={`1. Working with Files:`} />
+      <Text text={`Here are some basic commands used for managing files in Solaris:`} />
 
-      <div className='ml-4'>
-        <p className='mt-3 font-bold text-red-500'>2. To create a size file</p>
-        <p className=' font-bold ml-5 text-violet-600'> {`mkfile <file_size> <file_name>`} </p>
-        <img src={`${img_base_url}hdlfemirjoiyputyz3n2.png`} alt='' className='mt-5 mb-8 md:w-2xl lg:max-w-5xl' />
-      </div>
+      <Supersubheading text={`To create an empty file:`} />
+      <TerminalOutput content={`[root@localhost:~ ]# touch file.txt
+[root@localhost:~ ]# ls
+file.txt
+[root@localhost:~ ]#`} />
+      <Text text={`The \`touch\` command creates an empty file or updates the timestamp of an existing file.`} />
 
-      <div className='ml-4'>
-        <p className='mt-3 font-bold text-red-500'>3. To create a text file in editor mode</p>
-        <p className=' font-bold ml-5 text-violet-600'> {`vi <file_name>`} </p>
-        <img src={`${img_base_url}itob7nrph9jnhzqedtak.png`} alt='' className='mt-5 mb-8 md:w-2xl lg:max-w-5xl' />
-      </div>
+      <Supersubheading text={`To create a file of a specified size:`} />
+      <TerminalOutput content={`mkfile <file_size> <file_name>`} />
+      <Text text={`The \`mkfile\` command creates a file with the specified size (e.g., \`mkfile 1g myfile.txt\` creates a 1 GB file).`} />
 
-      <div className='ml-4'>
-        <p className='mt-3 font-bold text-red-500'> 4. To read a text file</p>
-        <p className=' font-bold ml-5 text-violet-600'> {`cat <file_name>`} </p>
-        <img src={`${img_base_url}ky8deydv4lcupvygawsy.png`} alt='' className='mt-5 mb-8 md:w-2xl lg:max-w-5xl' />
-      </div>
+      <Supersubheading text={`To create a text file in editor mode:`} />
+      <TerminalOutput content={`[root@localhost:~ ]# vi file3`} />
+      <Text text={`The \`vi\` command opens a file in the vi editor, allowing you to create and modify text files interactively.`} />
 
-      <div className='ml-4'>
-        <p className='mt-3 font-bold text-red-500'>5. To list down files</p>
-        <p className=' font-bold ml-5 text-violet-600'> {`ls`} </p>
-        <img src={`${img_base_url}h3o5hp4iiaujavtaykou.png`} alt='' className='mt-5 mb-8 md:w-2xl lg:max-w-5xl' />
-      </div>
+      <Supersubheading text={`To read the contents of a text file:`} />
+      <TerminalOutput content={`[root@localhost:~ ]# cat file.txt
+Hello world!
+[root@localhost:~ ]#`} />
+      <Text text={`The \`cat\` command displays the contents of the file in the terminal.`} />
 
-      <div className='ml-4'>
-        <p className='mt-3 font-bold text-red-500'>6. To list down files including hidden files</p>
-        <p className=' font-bold ml-5 text-violet-600'> {`ls -la`} </p>
-        <img src={`${img_base_url}uzlevdjrousravmm42sa.png`} alt='' className='mt-5 mb-8 md:w-2xl lg:max-w-5xl' />
-      </div>
-      <div className='ml-4'>
-        <p className='mt-3 font-bold text-red-500'>7. To list down all files with permission and size</p>
-        <p className=' font-bold ml-5 text-violet-600'> {`ls -larth`} </p>
-        <img src={`${img_base_url}f2vhc6xg2ukag2mbc7vf.png`} alt='' className='mt-5 mb-8 md:w-2xl lg:max-w-5xl' />
-      </div>
-      <div className='ml-4'>
-        <p className='mt-3 font-bold text-red-500'>8. To remove a file</p>
-        <p className=' font-bold ml-5 text-violet-600'> {`rm <file_name>`} </p>
-        <img src={`${img_base_url}wbo5qzsmxdmcygletec6.png`} alt='' className='mt-5 mb-8 md:w-2xl lg:max-w-5xl' />
-      </div>
-      <div className='ml-4'>
-        <p className='mt-3 font-bold text-red-500'> 9. To remove a file forcefully</p>
-        <p className=' font-bold ml-5 text-violet-600'> {`rm -rf <file_name>`} </p>
-        <img src={`${img_base_url}deaokyap4moix1mklx1t.png`} alt='' className='mt-5 mb-8 md:w-2xl lg:max-w-5xl' />
-      </div>
-      <div className='ml-4'>
-        <p className='mt-3 font-bold text-red-500'>10. To remove multiple files at a time </p>
-        <p className=' font-bold ml-5 text-violet-600'> {`rm -rf <file1> <file2> <file3>`} </p>
-        <img src={`${img_base_url}htqkac3ty4mxewijuoif.png`} alt='' className='mt-5 mb-8 md:w-2xl lg:max-w-5xl' />
-      </div>
+      <Supersubheading text={`To list files in the current directory:`} />
+      <TerminalOutput content={`[root@localhost:~ ]# ls
+file.txt  file3
+[root@localhost:~ ]#`} />
+      <Text text={`The \`ls\` command lists all files and directories in the current directory.`} />
 
-      <div className='ml-4'>
-        <p className='mt-3 font-bold text-red-500'>11. To copy a file</p>
-        <p className=' font-bold ml-5 text-violet-600'> {`cp <source_file> <destination>`} </p>
-        <img src={`${img_base_url}eva6bbeayfcdvx4gpplv.png`} alt='' className='mt-5 mb-8 md:w-2xl lg:max-w-5xl' />
-      </div>
-      <div className='ml-4'>
-        <p className='mt-3 font-bold text-red-500'>12. To move a file</p>
-        <p className=' font-bold ml-5 text-violet-600'> {`mv <source_file> <destination>`} </p>
-        <img src={`${img_base_url}n9xcdzsjx8i7ubdd9vsv.png`} alt='' className='mt-5 mb-8 md:w-2xl lg:max-w-5xl' />
-      </div>
-      <div className='ml-4'>
-        <p className='mt-3 font-bold text-red-500'>13. To rename a file</p>
-        <p className=' font-bold ml-5 text-violet-600'> {`mv <file_name> <new_name>`} </p>
-        <img src={`${img_base_url}b6iitabyjadilljn1uz1.png`} alt='' className='mt-5 mb-8 md:w-2xl lg:max-w-5xl' />
-      </div>
+      <Supersubheading text={`To list all files, including hidden ones:`} />
+      <TerminalOutput content={`[root@localhost:~ ]# ls -la
+total 68
+drwx------  6 root root  4096 Feb 12 18:38 .
+drwxr-xr-x 23 root root  4096 Feb 12 03:42 ..
+-rw-------  1 root root  8887 Feb 12 03:44 .bash_history
+-rw-r--r--  1 root root  3106 Apr 22  2024 .bashrc
+drwx------  2 root root  4096 Feb  7 13:49 .cache
+-rw-r--r--  1 root root    13 Feb 12 18:39 file.txt
+-rw-r--r--  1 root root     4 Feb 12 18:38 file3
+[root@localhost:~ ]#`} />
+      <Text text={`The \`-la\` option shows all files, including hidden ones (those starting with a dot), and provides detailed information like file permissions.`} />
 
-<hr className='my-5 ml-4'/>
-  {/* dir */}
-  <h2 className='ml-4 mr-2 mt-4 text-teal-500 font-bold text-xl'> Directories </h2>
-      <div className='ml-4'>
-        <p className='mt-3 font-bold text-red-500'>1. To create a directory</p>
-        <p className=' font-bold ml-5 text-violet-600'> {`mkdir <dir_name>`} </p>
-        <img src={`${img_base_url}pgrbjpkxx3gzy07ac9xg.png`} alt='' className='mt-5 mb-8 md:w-2xl lg:max-w-5xl' />
-      </div>
+      <Supersubheading text={`To list files with permissions and size in human-readable format:`} />
+      <TerminalOutput content={`[root@localhost:~ ]# ls -larth
+total 68K
+-rw-r--r--  1 root root  161 Apr 22  2024 .profile
+-rw-r--r--  1 root root 3.1K Apr 22  2024 .bashrc
+drwx------  2 root root 4.0K Jan  6 20:16 .ssh
+-rw-r--r--  1 root root    4 Feb 12 18:38 file3
+-rw-------  1 root root 9.9K Feb 12 18:38 .viminfo
+drwx------  6 root root 4.0K Feb 12 18:38 .
+-rw-r--r--  1 root root   13 Feb 12 18:39 file.txt
+[root@localhost:~ ]#`} />
+      <Text text={`The \`-larth\` option shows files sorted by modification time, with human-readable file sizes and detailed permissions.`} />
 
-      <div className='ml-4'>
-        <p className='mt-3 font-bold text-red-500'>2. To create a children directory</p>
-        <p className=' font-bold ml-5 text-violet-600'> {`mkdir -p <dir_name/chilid_dir/more_inner_dir>`} </p>
-        <img src={`${img_base_url}x4t4g7ro00l5b1rgpij2.png`} alt='' className='mt-5 mb-8 md:w-2xl lg:max-w-5xl' />
-      </div>
+      <Supersubheading text={`To remove a file:`} />
+      <TerminalOutput content={`[root@localhost:~ ]# rm file3
+[root@localhost:~ ]# ls
+file.txt
+[root@localhost:~ ]#`} />
+      <Text text={`The \`rm\` command removes a specified file from the filesystem.`} />
 
-      <div className='ml-4'>
-        <p className='mt-3 font-bold text-red-500'>3. To delete an empty directory</p>
-        <p className=' font-bold ml-5 text-violet-600'> {`rmdir <dir_name>`} </p>
-        <img src={`${img_base_url}kzxtmwjizewomqgztm92.png`} alt='' className='mt-5 mb-8 md:w-2xl lg:max-w-5xl' />
-      </div>
+      <Supersubheading text={`To forcefully remove a file:`} />
+      <TerminalOutput content={`[root@localhost:~ ]# rm -rf file.txt
+[root@localhost:~ ]# ls
+[root@localhost:~ ]#`} />
+      <Text text={`The \`-rf\` option forces the removal of the file or directory without confirmation (use with caution).`} />
 
-      <div className='ml-4'>
-        <p className='mt-3 font-bold text-red-500'>4. To delete a non-empty directory</p>
-        <p className=' font-bold ml-5 text-violet-600'> {`rm -rf <dir_name>`} </p>
-        <img src={`${img_base_url}ual8pigkhmjhhth1uegh.png`} alt='' className='mt-5 mb-8 md:w-2xl lg:max-w-5xl' />
-      </div>
+      <Supersubheading text={`To remove multiple files at once:`} />
+      <TerminalOutput content={`[root@localhost:~ ]# ls
+file1  file2  file3
+[root@localhost:~ ]# rm -rf file1 file2 file3
+[root@localhost:~ ]# ls
+[root@localhost:~ ]#`} />
+      <Text text={`You can delete multiple files simultaneously by specifying each file separated by a space.`} />
 
+      <Supersubheading text={`To copy a file:`} />
+      <TerminalOutput content={`[root@localhost:~ ]# cp file.txt /tmp
+[root@localhost:~ ]# ls /tmp
+file.txt
+[root@localhost:~ ]#`} />
+      <Text text={`The \`cp\` command copies a file from the source to the destination.`} />
 
+      <Supersubheading text={`To move a file:`} />
+      <TerminalOutput content={`[root@localhost:~ ]# mv file.txt /tmp/
+[root@localhost:~ ]# ls /tmp/
+file.txt
+[root@localhost:~ ]# ls
+[root@localhost:~ ]#`} />
+      <Text text={`The \`mv\` command moves a file from one location to another. It can also be used to rename files.`} />
 
+      <Supersubheading text={`To rename a file:`} />
+      <TerminalOutput content={`[root@localhost:~ ]# ls
+file1
+[root@localhost:~ ]# mv file1 file2
+[root@localhost:~ ]# ls
+file2
+[root@localhost:~ ]#`} />
+      <Text text={`To rename a file, use the \`mv\` command, specifying the current filename and the new name.`} />
+
+     <Line/>
+
+      {/* Directories */}
+      <Subheading text={`2. Working with Directories:`} />
+      <Text text={`Now, let's look at some basic directory commands used for managing directories in Solaris:`} />
+
+      <Supersubheading text={`To create a directory:`} />
+      <TerminalOutput content={`[root@localhost:~ ]# mkdir folder
+[root@localhost:~ ]# ls
+folder
+[root@localhost:~ ]#`} />
+      <Text text={`The \`mkdir\` command creates a new directory with the specified name.`} />
+
+      <Supersubheading text={`To create parent directories:`} />
+      <TerminalOutput content={`[root@localhost:~ ]# mkdir -p folder1/folder2
+[root@localhost:~ ]# ls
+folder1
+[root@localhost:~ ]# ls folder1/
+folder2
+[root@localhost:~ ]#`} />
+      <Text text={`The \`-p\` option allows creating nested directories (parent and child directories) in a single command.`} />
+
+      <Supersubheading text={`To delete an empty directory:`} />
+      <TerminalOutput content={`[root@localhost:~ ]# rmdir folder
+[root@localhost:~ ]# ls
+[root@localhost:~ ]#`} />
+      <Text text={`The \`rmdir\` command removes an empty directory.`} />
+
+      <Supersubheading text={`To delete a non-empty directory:`} />
+      <TerminalOutput content={`[root@localhost:~ ]# rm -rf folder1
+[root@localhost:~ ]# ls
+[root@localhost:~ ]#`} />
+      <Text text={`To delete a non-empty directory and its contents, use \`rm -rf\` (use with caution).`} />
     </div>
   );
 };
