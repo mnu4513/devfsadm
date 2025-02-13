47420ead29a8a2efcc684448339c24799cdf35e2
mnu4513 <mnu4513@gmail.com>
2025-02-13 11:38:10 +0530

    process management pending

--- src/components/solaris/ProcessManagement.js
+++ src/components/solaris/ProcessManagement.js
@@ -1,61 +1,46 @@
 import React from 'react';
-import { img_base_url } from '../Config';
+import { Heading, Supersubheading, Text, TerminalOutput } from "../utils/Comps";
 
 const ProcessManagement = () => {
-
-  const content = [{
-    id: 1,
-    title: 'To list all process',
-    command: ['ps-ef'],
-    image: 'yl036nfkbs61pqrnkpd8.png'
-  }, {
-    id: 2,
-    title: 'To search process',
-    command: ['ps -ef | grep -i <process_name>', 'ps -ef | grep -i <PID>', 'ps -ef | grep -i <user_name>'],
-    image: 'kx9vh4t5dhbwq8g4vczo.png'
-  }, {
-    id: 3,
-    title: 'To kill a process',
-    command: ['kill <PID>'],
-    image: 'ytcpeag3hr155k7i3guj.png'
-  }, {
-    id: 4,
-    title: 'To kill a process forcefully',
-    command: ['kill -9 <PID>'],
-    image: 'typui69pg3nbx7eiaqkt.png'
-  }, {
-    id: 5,
-    title: 'To get childern or parenet process of any process',
-    command: ['ptree <PID>'],
-    image: 'gsjh6knxsp3pcvp19mkd.png'
-  }];
-
   return (
-    <div className='mx-4 my-4 lg:max-w-7xl lg:mx-auto'>
-      <div>
-        <h2 className='text-2xl font-bold my-5 ml-4'>Process Management in Solaris</h2>
-        <p className='ml-4'>
-          In Solaris, process management involves activities related to the creation, execution, and termination of processes. Solaris, being a Unix-based operating system, follows Unix process management principles.
-        </p>
-      </div>
-      <div >
-        <iframe className='mt-5 h-96 w-full md:h-96 md:px-24 lg:w-4/5' src="https://www.youtube.com/embed/C49Tksl_3J4?si=4GLRa9_QsoRG_LGf" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-      </div>
-      <div >
-          {
-            content?.map((e) => {
-              return (
-                <div key={e.id} className='ml-4'>
-                  <h2 className=' mr-2 mt-4 font-bold'> {e.id}. {e.title} :-  </h2>
-                  {
-                    e.command?.map((x) => <p key={x} className='ml-4 font-bold'> {x} </p>)
-                  }
-                  <img src={`${img_base_url}${e.image}`} alt='' className='mt-3 mb-10 md:w-2xl lg:max-w-5xl' />
-                </div>
-              )
-            })
-          }
-      </div>
+    <div className='mx-4 my-4 lg:max-w-4xl lg:mx-auto'>
+      {/* Page Heading */}
+      <Heading text="Process Management in Solaris" />
+      
+      {/* Introduction */}
+      <Text text="In Solaris, process management involves activities related to the creation, execution, and termination of processes. Solaris, being a Unix-based operating system, follows Unix process management principles. This includes commands for viewing, searching, and controlling processes." />
+
+      {/* Command 1: To list all processes */}
+      <Supersubheading text="1. To list all processes" />
+      <TerminalOutput content="ps -ef" />
+      <Text text="The `ps -ef` command lists all the processes running on the system, along with detailed information such as Process ID (PID), Parent Process ID (PPID), CPU usage, and the command used to start the process. It is one of the most commonly used commands to get a snapshot of all active processes." />
+
+      {/* Command 2: To search for a process */}
+      <Supersubheading text="2. To search for a process" />
+      <TerminalOutput content="ps -ef | grep -i <process_name>" />
+      <Text text="The `ps -ef | grep -i <process_name>` command allows you to filter the list of processes to find a specific one by its name. The `-i` flag makes the search case-insensitive." />
+      
+      <TerminalOutput content="ps -ef | grep -i <PID>" />
+      <Text text="You can also use the process ID (PID) to search for a specific process. This can be useful if you know the PID but need more details about the process." />
+
+      <TerminalOutput content="ps -ef | grep -i <user_name>" />
+      <Text text="Additionally, you can filter processes by the user running them. This command helps in identifying all processes associated with a particular user." />
+
+      {/* Command 3: To kill a process */}
+      <Supersubheading text="3. To kill a process" />
+      <TerminalOutput content="kill <PID>" />
+      <Text text="The `kill <PID>` command is used to terminate a process by sending a signal to the specified process ID (PID). By default, this sends a `TERM` signal, which asks the process to terminate gracefully." />
+
+      {/* Command 4: To kill a process forcefully */}
+      <Supersubheading text="4. To kill a process forcefully" />
+      <TerminalOutput content="kill -9 <PID>" />
+      <Text text="The `kill -9 <PID>` command forcefully kills a process by sending the `KILL` signal. This signal cannot be ignored by the process, making it useful for terminating unresponsive processes." />
+
+      {/* Command 5: To get children or parent process of any process */}
+      <Supersubheading text="5. To get children or parent process of any process" />
+      <TerminalOutput content="ptree <PID>" />
+      <Text text="The `ptree <PID>` command displays the process tree of a given PID, showing its parent and child processes in a hierarchical structure. This is useful for understanding the relationship between processes and identifying which processes are spawned by others." />
+
     </div>
   );
 };
--- src/components/solaris/ServiceManagement.js
+++ src/components/solaris/ServiceManagement.js
@@ -12,61 +12,61 @@ const ServiceManagement = () => {
       
 
       {/* Command 1: To list all services */}
-      <div className='ml-4'>
+      
         <Supersubheading text="1. To list all services" />
         <TerminalOutput content="svcs -a" />
         <Text text="The `svcs -a` command lists all services available on the system, regardless of their current status (online, offline, or maintenance)." />
-        </div>
+        
 
       {/* Command 2: To search a service using service name */}
-      <div className='ml-4'>
+      
         <Supersubheading text="2. To search a service using service name" />
         <TerminalOutput content="svcs -a | grep <service_name>" />
         <Text text="The `svcs -a | grep <service_name>` command filters the service list to search for a particular service by its name." />
-        </div>
+        
 
       {/* Command 3: To enable a service */}
-      <div className='ml-4'>
+      
         <Supersubheading text="3. To enable a service" />
         <TerminalOutput content="svcs enable <service_name>" />
         <Text text="The `svcs enable <service_name>` command enables a specific service, allowing it to start automatically on boot or when required." />
-        </div>
+        
 
       {/* Command 4: To disable a service */}
-      <div className='ml-4'>
+      
         <Supersubheading text="4. To disable a service" />
         <TerminalOutput content="svcadm disable <service_name>" />
         <Text text="The `svcadm disable <service_name>` command disables a service, preventing it from running even if it is triggered." />
-        </div>
+        
 
       {/* Command 5: To refresh a service */}
-      <div className='ml-4'>
+      
         <Supersubheading text="5. To refresh a service" />
         <TerminalOutput content="svcadm refresh <service_name>" />
         <Text text="The `svcadm refresh <service_name>` command refreshes the state of the service, reloading its configuration and restarting it without needing a full reboot." />
-        </div>
+        
 
       {/* Command 6: To restart any service */}
-      <div className='ml-4'>
+      
         <Supersubheading text="6. To restart any service" />
         <TerminalOutput content="svcadm restart <service_name>" />
         <Text text="The `svcadm restart <service_name>` command restarts a service. This can be useful to apply changes or resolve issues without rebooting the system." />
-        </div>
+        
 
       {/* Command 7: To list down services with extended information */}
-      <div className='ml-4'>
+      
         <Supersubheading text="7. To list down services with extended information" />
         <TerminalOutput content="svcs -xv" />
         <Text text="The `svcs -xv` command lists all services with detailed status and troubleshooting information, including which services are in maintenance mode and why." />
-        </div>
+        
 
       {/* Command 8: To clear any service from maintenance mode */}
-      <div className='ml-4'>
+      
         <Supersubheading text="8. To clear any service from maintenance mode" />
         <TerminalOutput content="svcadm clear <service_name>" />
         <Text text="The `svcadm clear <service_name>` command clears a service from maintenance mode and attempts to restart it. This is helpful if the service has encountered an issue and is stuck in maintenance." />
+        
         </div>
-    </div>
   );
 };
 
