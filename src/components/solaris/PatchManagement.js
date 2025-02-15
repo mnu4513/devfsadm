2ef0ad000b68429ed04a1df1fa94d86fa1bcccb7
mnu4513 <mnu4513@gmail.com>
2025-02-15 13:21:10 +0530

    patching pending

--- src/components/solaris/PatchManagement.js
+++ src/components/solaris/PatchManagement.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Heading, Subheading, Supersubheading, TerminalOutput, Text } from "../utils/Comps";
+import { Heading, Line, Subheading, Supersubheading, TerminalOutput, Text } from "../utils/Comps";
 
 const PatchingProcess = () => {
   return (
@@ -9,23 +9,24 @@ const PatchingProcess = () => {
       <Text text={`Patching in Solaris is a critical procedure used to keep the operating system up-to-date with the latest security patches, bug fixes, and system improvements. The patching process involves using Oracle-provided patch bundles and applying them using commands like 'pkg' to update system packages. There are two main types of patches: Base Repo Patch and SRU (Support Repository Update) Patch.`} />
 
       <Supersubheading text={`1. Base Repo Patch`} />
-      <Text text={`Base Repo Patch refers to the core patch repository, which contains the essential patches for Solaris OS. These patches are usually more general updates to the Solaris environment.`} />
+      <Text text={`Base Repo Patch refers to the core patch repository, which contains the essential patches for Solaris OS. These patches are usually more general updates to the Solaris environment, such as security updates, kernel fixes, or enhancements.`} />
 
       <Supersubheading text={`2. SRU Patch`} />
-      <Text text={`SRU patches are specific patches that are used for support and stability updates, and they are typically provided in the form of SRU bundles. They contain updates for specific components of the system that need fixes or improvements.`} />
+      <Text text={`SRU patches are specific patches that are used for support and stability updates. These patches address particular system components and offer fixes or improvements. They are bundled into SRU files that can be downloaded from Oracle's My Oracle Support (MOS).`}/>
 
       <Text text={`Before beginning the patching process, we need to perform several preparatory steps, such as checking the system state, identifying the patch versions, and ensuring the server is ready for patching.`} />
 
-      
-
-      <Subheading text={`Steps to patching process in solaris:`} />
+      <Line />
+      <Subheading text={`Steps to patching process in Solaris:`} />
 
       <Supersubheading text={`Step 1: Prechecks`} />
       <Text text={`Before applying patches, it’s essential to perform prechecks on the server. This helps ensure that the system is in a good state before proceeding with the patching process. Some of the prechecks include verifying the server’s current version, available disk space, and confirming that the server is running without any critical errors.`} />
+      <Text text={`For example, use the following commands to check the current version of Solaris and disk space:`} />
+      <TerminalOutput content={`[root@localhost:~]# uname -a`} />
+      <TerminalOutput content={`[root@localhost:~]# df -h`} />
 
       <Supersubheading text={`Step 2: Downloading Patch Bundle`} />
-      <Text text={`After completing the prechecks, the next step is to download the patch bundle for either Base Repo or SRU Repo from Oracle's My Oracle Support (MOS). To download the patches, you need an active support contract with Oracle.`} />
-
+      <Text text={`After completing the prechecks, the next step is to download the patch bundle for either Base Repo or SRU Repo from Oracle's My Oracle Support (MOS). You will need an active support contract with Oracle to download the patches.`} />
       <Text text={`Once the patches are downloaded, use FTP tools like WinSCP, FileZilla, or any other FTP/SFTP tool to copy the downloaded patch bundle to the Solaris server.`} />
 
       <Text text={`After transferring the patch bundle, extract the contents of the installation guide.`} />
@@ -90,6 +91,27 @@ const PatchingProcess = () => {
       <Text text={`For example, you can check the package status using:`} />
       <TerminalOutput content={`[root@localhost:~]# pkg info`} />
       <Text text={`You can also verify that the patches were successfully applied by checking the installed versions or running health checks on the system.`} />
+
+      <Line />
+      <Subheading text={`Roll Back to Previous Version`} />
+      <Text text={`If you encounter issues with the new patch, or if you want to roll back to the previous version, you can revert to a previous boot environment (BE) using the following steps:`} />
+
+      <Supersubheading text={`Condition 1: If Application Has Issues with New OS Patch`} />
+      <Text text={`First, check the previous boot environment status using the command:`} />
+      <TerminalOutput content={`[root@localhost:~]# beadm list`} />
+      <Text text={`Next, select the previous boot environment and activate it:`} />
+      <TerminalOutput content={`[root@localhost:~]# beadm activate be_name`} />
+      <Text text={`It will set the flag 'R' on the previous BE. After this, reboot the server to activate the previous BE:`} />
+      <TerminalOutput content={`[root@localhost:~]# init 6`} />
+
+      <Supersubheading text={`Condition 2: If the System Is Not Booting or Not Coming Up`} />
+      <Text text={`If the system is stuck and is not booting, you can access the system via console login. The system will show the 'ok >' prompt.`} />
+      <TerminalOutput content={`ok >`} />
+      <Text text={`From here, list down all the available boot environments using the following command:`} />
+      <TerminalOutput content={`ok > boot -L`} />
+      <Text text={`It will show all available boot environments. You can then choose and boot from the previous boot environment using the following command:`} />
+      <TerminalOutput content={`ok > boot BE_name`} />
+      <Text text={`This will boot the system with the previously functional BE.`} />
 
     </div>
   );
