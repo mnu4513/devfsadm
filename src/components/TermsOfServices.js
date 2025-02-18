e26f91dbf8313fbf70afa52f828321257a5c154c
mnu4513 <mnu4513@gmail.com>
2025-02-18 08:15:35 +0530

    .

--- src/components/TermsOfServices.js
+++ src/components/TermsOfServices.js
@@ -1,55 +1,42 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { Heading, Text, Subheading, TextWithLinks } from './utils/Comps';
 
 const TermsOfServices = () => {
     return (
-        <div className="container mx-auto mt-8 px-4 lg:max-w-7xl mb-5">
-            <h1 className="text-3xl text-red-500 font-semibold mb-4">Terms of Service</h1>
+        <div className="container mx-auto mt-8 px-4 lg:max-w-4xl mb-5">
+            <Heading text={`Terms of Service`}/>
+           
 
-            <p className='mb-2'>
-                Last updated: 05/03/2024
-            </p>
+           
+<Text text={`Last updated: 05/03/2024`}/>
 
+            <Subheading text={`Acceptance of Terms`}/>
+            <Text text={`Welcome to infoUnix! By using our website and services, you agree to comply with and be bound by the following terms and conditions. If you do not agree to these terms, please do not use our services.`}/>
 
+            
 
-            <h2 className="text-xl text-red-500 font-semibold mb-2 mt-4">Acceptance of Terms</h2>
-
-            <p className='mb-2'>
-                Welcome to DevFsAdm! By using our website and services, you agree to comply with and be bound by the following terms and conditions. If you do not agree to these terms, please do not use our services.
-            </p>
-
-
-            <h2 className="text-xl text-red-500 font-semibold mb-2 mt-4">Description of Services</h2>
-
-            <p className='mb-2'>
-                DevFsAdm offers free online courses in Solaris, Linux administration, HTML, CSS, JavaScript, and Node.js programming. These courses include video tutorials, written content, and other educational resources. Our services are provided free of charge.
-            </p>
+            <Subheading text={`Description of Services`}/>
+            <Text text={`infoUnix offers free online courses in Solaris adminstration, Linux administration, Git & Github, Docker, Podman, HTML, CSS, JavaScript, and Node.js programming. These courses include video tutorials, written content, and other educational resources. Our services are provided free of charge.`}/>
+       
 
 
-
-            <h2 className="text-xl text-red-500 font-semibold mb-2 mt-4">User Responsibilities</h2>
-
-            <p className='mb-2'>
-                You are responsible for your use of our services. This includes maintaining the confidentiality of your account information and being aware of and compliant with applicable laws and regulations.
-            </p>
+            <Subheading text={`User Responsibilities`}/>
+            <Text text={`You are responsible for your use of our services. This includes maintaining the confidentiality of your account information and being aware of and compliant with applicable laws and regulations.`}/>
+      
 
 
-
-            <h2 className="text-xl text-red-500 font-semibold mb-2 mt-4">Course Content</h2>
-
-            <p className='mb-2'>
-                The course content provided on DevFsAdm is intended for educational purposes only. We make no guarantees regarding the accuracy, completeness, or suitability of the information. Courses and content may be updated, modified, or discontinued without prior notice.
-            </p>
+            <Subheading text={`Course Content`}/>
+            <Text text={`The course content provided on infoUnix is intended for educational purposes only. `}/>
+            <Text text={`We make no guarantees regarding the accuracy, completeness, or suitability of the information. Courses and content may be updated, modified, or discontinued without prior notice.`}/>
+        
 
 
+            <Subheading text={`User Conduct`}/>
+            <Text text={`While using our services, you agree not to engage in activities that may:`}/>
 
 
-            <h2 className="text-xl text-red-500 font-semibold mb-2 mt-4">User Conduct</h2>
-
-            <p className='mb-2'>
-                While using our services, you agree not to engage in activities that may:
-            </p>
-            <ul>
+            <ul className='font-cambria list-disc ml-4'>
                 <li>
                     Violate any laws or regulations.
                 </li>
@@ -66,53 +53,36 @@ const TermsOfServices = () => {
 
 
 
-
-            <h2 className="text-xl text-red-500 font-semibold mb-2 mt-4">Privacy</h2>
-
-            <p className='mb-2'>
-                We respect your privacy. Our <Link to="/privacy-policy" className='text-blue-500 mr-1'>
+            <Subheading text={`Privacy`}/>
+            <TextWithLinks>We respect your privacy. Our <Link to="/privacy-policy" className='text-blue-500 mr-1'>
                     Privacy Policy
                 </Link>
-                outlines how we collect, use, and protect your personal information.
-            </p>
+                outlines how we collect, use, and protect your personal information.</TextWithLinks>
+            
 
 
 
-
-            <h2 className="text-xl text-red-500 font-semibold mb-2 mt-4">Termination of Services</h2>
-
-            <p className='mb-2'>
-                We reserve the right to terminate or suspend your access to our services at any time, with or without cause and without notice.
-            </p>
+            <Subheading text={`Termination of Services`}/>
+            <Text text={`We reserve the right to terminate or suspend your access to our services at any time, with or without cause and without notice.`}/>
+           
 
 
 
-
-            <h2 className="text-xl text-red-500 font-semibold mb-2 mt-4">
-                Changes to Terms
-            </h2>
-
-            <p className='mb-2'>
-                We may update or modify these terms from time to time. You will be notified of significant changes, but it is your responsibility to review and comply with the latest terms.
-            </p>
+            <Subheading text={`Changes to Terms`}/>
+            <Text text={`We may update or modify these terms from time to time. You will be notified of significant changes, but it is your responsibility to review and comply with the latest terms.`}/>
+            
 
 
 
-
-            <h2 className="text-xl text-red-500 font-semibold mb-2 mt-4">Intellectual Property</h2>
-
-            <p className='mb-2'>
-                All content provided on DevFsAdm, including text, images, videos, and course materials, is the intellectual property of DevFsAdm. Unauthorized use or reproduction is prohibited.
-            </p>
-
-
-            <h2 className="text-xl text-red-500 font-semibold mb-2 mt-4">Contact Information</h2>
-
-            <p className='mb-2'>
-                If you have any questions or concerns about these terms, please contact us at <a href="mailto:contact@devfsadm.com" className='text-blue-500'>contact@devfsadm.com</a>.
-
-                Thank you for choosing DevFsAdm for your online learning journey!
-            </p>
+            <Subheading text={`Intellectual Property`}/>
+            <Text text={`All content provided on infoUnix, including text, images, videos, and course materials, is the intellectual property of infoUnix. Unauthorized use or reproduction is prohibited.`}/>
+           
+            <Subheading text={`Contact Information`}/>
+            <TextWithLinks>
+            If you have any questions or concerns about these terms, please contact us at <a href="mailto:support@infounix.com" className='text-blue-500'>support@infounix.com</a>.
+            </TextWithLinks>
+            <Text text={`Thank you for choosing infoUnix for your online learning journey!`}/>
+           
 
         </div>
     );
