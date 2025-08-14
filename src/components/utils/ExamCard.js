c9e759e54254cacbfd54213d280c0f39223a426b
mnu4513 <mnu4513@gmail.com>
2025-08-15 01:19:24 +0530

    .

--- src/components/utils/ExamCard.js
+++ src/components/utils/ExamCard.js
@@ -3,7 +3,7 @@ import { Text, Line, Command, Supersubheading } from './Comps';
 
 const ExamCard = ({ question }) => {
   return (
-    <div className=" rounded shadow-sm bg-[#1e293b] p-">
+    <div className=" rounded shadow-sm bg-[#1e293b] text-white p-">
       {/* Displaying the question ID/Number */}
       <Supersubheading text={`🖊️ Question: ${question.id}`} />
 
